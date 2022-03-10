57b733fd0c3d236ef353b5182ae6e9dfeb920fab
Nadia Najjar <nanajjar@uncc.edu>
2022-03-10 17:52:51 -0500

    naming update

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"BookStore"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"bookstore"}];updateSearchResults();
