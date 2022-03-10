e5558c2a86e0327c4020296af152ed658f71c3f3
Nadia Najjar <nanajjar@uncc.edu>
2022-03-10 18:04:06 -0500

    clean

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"bookstore","l":"Book"},{"p":"bookstore","l":"Bookstore"},{"p":"bookstore","l":"BookStoreDriver"},{"p":"bookstore","l":"CD"},{"p":"bookstore","l":"DVD"},{"p":"bookstore","l":"Member"},{"p":"bookstore","l":"PremiumMember"}];updateSearchResults();
